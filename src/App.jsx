1003a2c96c291c9c82ed479ed349c2eca58a92e0
softshailesh <shailesh.p@singhsoft.com>
2025-09-01 10:43:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="w-50 h-50 border-2 border-black flex justify-center items-center">
+          <h1 className="bg-red-500">Vite + React</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
